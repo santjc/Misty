e4c76813bf77e84558bb50635bbb7b85f680ae02
Santiago Colombatto <sjcolombatto@hotmail.com>
2020-10-15 10:03:13 -0300

    New parallax , new bg

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour {
 
-    private Transform cam;
-    private Vector3 lastCameraPos;
-    public float parallaxEffect = .5f;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    [SerializeField] private float parallaxMultiplier = .5f;
     private float textureUnitSizeX;
 
-    private void Start () {
-        cam = Camera.main.transform;
-        lastCameraPos = cam.position;
-        Sprite sprite = GetComponent<SpriteRenderer> ().sprite;
+    private void Start() {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         Texture2D texture = sprite.texture;
         textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
-    private void LateUpdate () {
-        Vector3 deltaMovement = cam.position - lastCameraPos;
-        transform.position += deltaMovement * parallaxEffect;
-        lastCameraPos = cam.position;
-        
+    private void Update() {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * parallaxMultiplier;
+        lastCameraPosition = cameraTransform.position;
 
-        if ( Mathf.Abs(cam.position.x - transform.position.x) >= textureUnitSizeX) {
-            float offsetPositionX = (cam.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3 (cam.position.x + offsetPositionX, transform.position.y, 0);
+        if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX){
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3 (cameraTransform.position.x , transform.position.y);
         }
     }
 }
